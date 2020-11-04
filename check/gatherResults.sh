387c4ae8f3450dd383eb26a0ba47b5e1d7d5f240
slasom <slasom@unex.es>
2020-11-04 11:02:36 +0100

    Added check UI tests results

--- check/gatherResults.sh
+++ check/gatherResults.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
 for file in logs/*.txt; do
-  #ilename=$(echo "${file##*/}")
-  #echo "$file"
-
-  # cp "$file" "$(basename "$file" .txt).json"
    tail -7 $file > logs/"$(basename "$file" .txt).json"
-  # if [[ $filename == *"result"* ]]; then
-  # 	echo "It's there!"
-  	
-  #fi
-
 done
--- check/index.js
+++ check/index.js
@@ -9,6 +9,7 @@ var rootPath=path.normalize(__dirname+'/../..');;
 
 var resultFileName;
 var result = {};
+var success = true;
 
 
 var configFileName = "perses-tests.yaml";
@@ -35,15 +36,16 @@ try {
 
 tests.forEach(function(value){
   if(value.type==="apipecker"){
+    console.log("Checking performance tests...")
     resultFileName = "./logs/results"+(value.id).replace(/\s/g, '')+".json"
     try {
       result = JSON.parse(fs.readFileSync(resultFileName, 'utf8'));
       
       if (result.mean > value.expect.mean.under){
-        console.error("TEST FAILED: result > max --> "+result.mean+ ">"+value.expect.mean.under);
+        console.error(value.id +": FAILED: result > max --> "+result.mean+ ">"+value.expect.mean.under);
         process.exit(1);
       }else{
-        console.log("TEST PASSED: result < max --> "+result.mean+ " > "+value.expect.mean.under);
+        console.log(value.id +": PASSED: result < max --> "+result.mean+ " > "+value.expect.mean.under);
       }    
   
     } catch (e) {
@@ -52,8 +54,57 @@ tests.forEach(function(value){
     }
 
   }
+  if(value.type==="espresso"){
+    console.log("Checking espresso tests...")
+    checkUIResults()
+  
+  }
+
     
 });
+
+
+async function checkUIResults(){
+  fs.readdir(path.join('./logs/devices-logs/espresso/'), function(err, filenames) {
+    if (err) {
+      return console.log(err);
+    }
+    
+    files=filenames.length
+    filenames.forEach(function(filename) {
+
+      file=fs.readFileSync('./logs/devices-logs/espresso/' + filename, 'utf-8');
+
+
+      //    TODO: CATCH TIME 
+        let arr = file.split(/\r?\n/);
+        arr.forEach((line, idx)=> {
+            
+          if(line.includes("OK")){
+            console.log("Results of "+filename)
+            console.log(line)
+            //Time
+            console.log(arr[idx-2])
+          }
+
+          if(line.includes("FAILURES")){
+            console.log("Results of "+filename)
+            console.log(line)
+            console.log(arr[idx+1])
+            success=false;
+          }
+        });
+
+        console.log("-------------")
+    });
+    if(!success){
+      console.error("FAILURE: Espresso tests have not been passed!");
+      process.exit(1);
+    }else{
+      console.log("SUCCESS: Espresso tests have been passed correctly!")
+    }
+  });
+}
 
 
 
